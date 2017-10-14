1b2bf716d9f55b50d5a49238710cabad7d32560a
Mac User <laser_yellow@cnc.jp>
2017-10-14 18:29:51 +0900

    build 通る

--- fuse.js
+++ fuse.js
@@ -1,29 +1,27 @@
-const {FuseBox} = require('fuse-box');
-const argv      = require('yargs').argv;
- 
+const { FuseBox } = require('fuse-box');
+const argv = require('yargs').argv;
+
 const dev = argv.variant === 'dev';
- 
+
 const fuse = FuseBox.init({
     homeDir: 'src/scripts',
-    output: 'public/assets/$name.js',
+    output: 'public/build/$name.js',
     sourceMaps: dev,
-    cache: dev
+    cache: dev,
+    tsConfig: "tsconfig.json"
 });
 
-if(dev){
-  fuse.dev({
-    port: 8080,                       // port 番号を指定。デフォルトは 4444
-    root: 'public/',                  // ルートディレクトリを指定。デフォルトは output 先となるディレクトリ
-    open: true,                       // ブラウザで表示
-    httpServer: true,                 // http サーバ機能を有効化するかどうかを指定。デフォルトは true
-    socketURI: 'ws://localhost:3333'  // 
-  });
-}
-else{
-  fuse.bundle('app')
-      .instructions(`> main.ts`);
+if (dev) {
+    fuse.dev({
+        port: 8080, // port 番号を指定。デフォルトは 4444
+        open: true, // ブラウザで表示
+        httpServer: true, // http サーバ機能を有効化するかどうかを指定。デフォルトは true
+    });
 }
 
-dev && fuse.watch();
- 
+const app = fuse.bundle('app')
+    .instructions(`> main.ts`);
+
+dev && app.watch().hmr();
+
 fuse.run();

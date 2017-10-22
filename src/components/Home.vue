70f2210bbe321a2c8e404e0e298f595480b62e94
Mac User <laser_yellow@cnc.jp>
2017-10-22 22:11:02 +0900

    player をコンポーネント化。外部から操作してみる

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -2,28 +2,59 @@
   <div>
     <a-scene>
       <a-cylinder position="0 0 0" radius="20" height="0" color="#FFC65D"></a-cylinder>
-      <a-box :position="{x: x, y: y, z: z}" color="#FFFFFF"></a-box>
+
+      <player v-for="player in players" v-bind:key="player.id" :x="player.x" :z="player.z" color="#FFFFFF">
+      </player>
       <a-sky color="#000000"></a-sky>
     </a-scene>
 
-    <v-btn flat @click.stop="random()" style="position:absolute; z-index:20; top:0px;">Push</v-btn>
+    <div id="input">
+      <input v-model="inputID">
+      <input v-model="inputX">
+      <input v-model="inputZ">
+      <v-btn flat @click.stop="input()">Push</v-btn>
+    </div>
 
   </div>
 </template>
 <script lang="ts">
+import Player from "./Player.vue";
 export default {
+  components: {
+    Player
+  },
   data: function() {
     return {
-      x: 0,
-      y: 0.5,
-      z: 0
+      players: [],
+      inputID: "",
+      inputX: "",
+      inputZ: ""
     }
   },
   methods: {
-    random() {
-      console.log("random clicked")
-      this.x = Math.random();
-      this.z = Math.random();
+    input() {
+      var self = this;
+      var filtered = this.players.filter(function(object: any) {
+        return object.id == self.inputID;
+      });
+      var player = filtered[0];
+      if (player != null && player != undefined) {
+        console.log("x:" + this.inputX + " z:" + this.inputZ)
+        player.x = Number(this.inputX);
+        player.z = Number(this.inputZ);
+      }
+    }
+  },
+  created: function() {
+    this.players = [];
+    for (var i: number = 0; i < 21; i++) {
+      var num = i - 10;
+      this.players.push({
+        id: "hoge" + i,
+        name: "hogeName" + i,
+        x: num,
+        z: num
+      });
     }
   }
 }
@@ -44,5 +75,11 @@ export default {
     margin: 20px auto;
     height: 100px;
   }
+}
+
+#input {
+  position: absolute;
+  z-index: 20;
+  top: 0px;
 }
 </style>
---
+++ src/components/Player.vue
@@ -0,0 +1,32 @@
+<template>
+  <a-box color="#FFFFFF" :position="{x: x, y: 0.5, z: z}">
+  </a-box>
+</template>
+
+<script lang="ts">
+export default {
+  data: function() {
+    return {
+      id: "",
+      name: ""
+    }
+  },
+  props: {
+    x: Number,
+    z: Number
+  },
+  methods: {
+    move(x: number, z: number) {
+      this.x = x;
+      this.z = z;
+    }
+  },
+  created: function() {
+    console.log("Player created!!")
+  }
+}
+</script>
+
+<style>
+
+</style>
